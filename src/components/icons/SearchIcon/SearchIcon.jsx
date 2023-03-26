f64b797053465458fd8c65bbeee4a2e453d71b62
ankkarp <anna.a.k.2002@gmail.com>
2023-03-26 22:34:36 +0300

    init project and first try on video upload

---
+++ src/components/icons/SearchIcon/SearchIcon.jsx
@@ -0,0 +1,22 @@
+const SearchIcon = ({ width, height }) => {
+  return (
+    <svg viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M25.5 25.5L33 33"
+        stroke="#D8D80A"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3 15.8571C3 22.9579 8.75634 28.7143 15.8571 28.7143C19.4136 28.7143 22.6331 27.2702 24.9606 24.9364C27.2803 22.6108 28.7143 19.4014 28.7143 15.8571C28.7143 8.75634 22.9579 3 15.8571 3C8.75634 3 3 8.75634 3 15.8571Z"
+        stroke="#D8D80A"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SearchIcon;
