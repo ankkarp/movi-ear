0b3027c26fcf566d6e93e1b7c9f222dfba9a4255
ankkarp <anna.a.k.2002@gmail.com>
2023-03-27 11:18:55 +0300

    video upload

--- src/components/blocks/VideoInput/VideoInput.jsx
+++ src/components/blocks/VideoInput/VideoInput.jsx
@@ -2,6 +2,7 @@ import styles from "./VideoInput.module.css";
 
 import { useState, useRef } from "react";
 import UploadIcon from "@/components/icons/UploadIcon/UploadIcon";
+import http from "../../../api/http-common";
 
 export default function VideoInput({ width, height }) {
   const [source, setSource] = useState();
@@ -9,6 +10,21 @@ export default function VideoInput({ width, height }) {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
+    let formData = new FormData();
+    formData.append("file", file);
+
+    http.post("upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      // onUploadProgress,
+    });
+    // axios.post("upload", formData, {
+    //   headers: {
+    //     "Content-Type": "multipart/form-data",
+    //   },
+    // });
+
     const url = URL.createObjectURL(file);
     setSource(url);
   };
@@ -20,7 +36,10 @@ export default function VideoInput({ width, height }) {
   return (
     <div className={styles.container}>
       {source ? (
-        <video controls src={source} />
+        <>
+          <video controls src={source} />
+          {source}
+        </>
       ) : (
         <button onClick={handleChoose} className={styles.upload}>
           <input
